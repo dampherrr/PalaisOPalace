fa6cb5ce28fd544b2d9465d32458be467ac02fee
Geoffrey PECHIN <geoffrey.pechin@gmail.com>
2024-07-25 17:14:37 +0200

    add request for google sheet in app.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,33 @@
+import { useLoaderData } from "react-router-dom";
 import React from "react";
-
-
+import Papa from "papaparse";
 import "./App.css";
 
-
-
 function App() {
+  
+  const grotteMaison = useLoaderData();
+
+  const parse = () =>
+    Papa.parse(grotteMaison, {
+      header: true,
+      complete: (result) => result,
+      error: (error) => console.info(error),
+    });
+
+  const { data } = parse()
+
+  console.info(data);
+
   return (
     <>
-    <h1>coucou</h1>
-    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus vitae iste corporis vero ut exercitationem dolorem voluptatem rerum voluptates repudiandae, corrupti sequi asperiores nisi voluptatibus earum fugit nobis eos nesciunt!</p>
-    
+      <h1>coucou</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus vitae
+        iste corporis vero ut exercitationem dolorem voluptatem rerum voluptates
+        repudiandae, corrupti sequi asperiores nisi voluptatibus earum fugit
+        nobis eos nesciunt!
+      </p>
+      
     </>
   );
 }
