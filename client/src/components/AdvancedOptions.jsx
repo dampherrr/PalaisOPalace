e75f2d1037ff16e9b08f55ccef281402ed4ff9aa
ANTOINE FOUGA <antoine.fouga1@gmail.com>
2024-07-25 23:36:15 +0200

    need other branch 🚧

--- client/src/components/AdvancedOptions.jsx
+++ client/src/components/AdvancedOptions.jsx
@@ -3,7 +3,8 @@ import { useState } from "react";
 import "../styles/advancedOptions.css";
 
 export default function AdvancedOptions() {
-  const [inputsentence, setInputSentence] = useState("");
+  const [search, setSearch] = useState("");
+
   const [troc, setTroc] = useState("");
 
   const validate = () => {
@@ -13,30 +14,68 @@ export default function AdvancedOptions() {
     }
   };
 
+  const searchRequest = (event) => {
+    setSearch(event.target.value);
+  };
+
   return (
     <div className="select-container">
       <div className="input-selection">
-        <input type="text" placeholder="Input sentence" maxLength={30} />
-        <input type="text" placeholder="TROC*" maxLength={10} />
+        <input
+          type="text"
+          placeholder="Input sentence"
+          maxLength={30}
+          onChange={searchRequest}
+        />
+        <input
+          type="text"
+          placeholder="TROC*"
+          maxLength={10}
+          onChange={searchRequest}
+        />
       </div>
       <p className="devise">*Uniquement en 🐗 ou 🥪</p>
       <div className="container-options-button">
-        <button type="button" className="option-button">
+        <button
+          type="button"
+          className="option-button"
+          onClick={() => selectOptions("Trou")}
+        >
           Trou
         </button>
-        <button type="button" className="option-button">
+        <button
+          type="button"
+          className="option-button"
+          onClick={() => selectOptions("Trou sans fenêtre")}
+        >
           Trou sans fenêtre
         </button>
-        <button type="button" className="option-button">
+        <button
+          type="button"
+          className="option-button"
+          onClick={() => selectOptions(" Grotte Fienté")}
+        >
           Grotte Fienté
         </button>
-        <button type="button" className="option-button">
+        <button
+          type="button"
+          className="option-button"
+          onClick={() => selectOptions("   Grotte Moute")}
+        >
           GrotteMoute
         </button>
-        <button type="button" className="option-button">
+        <button
+          type="button"
+          className="option-button"
+          onClick={() => selectOption(" Caverne Familial")}
+        >
           Caverne Familial
         </button>
-        <button type="button" className="option-button">
+        <button
+          type="button"
+          className="option-button"
+          onClick={() => selectOption(" Fosse Commune")}
+        >
           Fosse Commune
         </button>
       </div>
