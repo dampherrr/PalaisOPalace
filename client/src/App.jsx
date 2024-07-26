afc7a10db8b1c7e887983a889882882be81777c4
Mathieu REY <mathieu.rey33@gmail.com>
2024-07-26 09:08:38 +0200

    ajout des composants Night , Light , des pages night.css et light.css ainsi que des images associées

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,34 +1,45 @@
-import { useLoaderData } from "react-router-dom";
-import React from "react";
-import Papa from "papaparse";
+import React, { useState } from "react";
+import Light from "./components/Light";
+import Night from "./components/Night";
 import "./App.css";
 
 function App() {
-  
-  const grotteMaison = useLoaderData();
+  const [currentScene, setCurrentScene] = useState("light");
 
-  const parse = () =>
-    Papa.parse(grotteMaison, {
-      header: true,
-      complete: (result) => result,
-      error: (error) => console.info(error),
-    });
-
-  const { data } = parse()
-
-  console.info(data);
+  const handleSceneChange = (event) => {
+    setCurrentScene(event.target.value);
+  };
 
   return (
-    <>
-      <h1>coucou</h1>
-      <p>
-        Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus vitae
-        iste corporis vero ut exercitationem dolorem voluptatem rerum voluptates
-        repudiandae, corrupti sequi asperiores nisi voluptatibus earum fugit
-        nobis eos nesciunt!
-      </p>
-      
-    </>
+    <div
+      className={`app-body ${
+        currentScene === "light" ? "light-body" : "night-body"
+      }`}
+    >
+      <select
+        className="select-container"
+        onChange={handleSceneChange}
+        value={currentScene}
+      >
+        <option value="light">Light</option>
+        <option value="night">Night</option>
+      </select>
+
+      <div className={currentScene === "light" ? "light-scene" : "night-scene"}>
+        {currentScene === "light" && <Light />}
+        {currentScene === "night" && <Night />}
+      </div>
+
+      <div className="container-body">
+        <h1>Palais Ô Palace</h1>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Possimus
+          vitae iste corporis vero ut exercitationem dolorem voluptatem rerum
+          voluptates repudiandae, corrupti sequi asperiores nisi voluptatibus
+          earum fugit nobis eos nesciunt!
+        </p>
+      </div>
+    </div>
   );
 }
 
---
+++ client/src/components/Night.jsx
@@ -0,0 +1,65 @@
+import React, { useEffect } from "react";
+import "../styles/night.css";
+import cloud1 from "../assets/media/pictures/cloud1.png";
+import cloud2 from "../assets/media/pictures/cloud2.png";
+import cloud3 from "../assets/media/pictures/cloud3.png";
+import forest from "../assets/media/pictures/forest.png";
+import fireTorch from "../assets/media/pictures/fire-torch-png.webp";
+
+const Night = () => {
+  useEffect(() => {
+    const createStars = () => {
+      const count = 500;
+      const scene = document.querySelector(".night-scene .scene");
+      if (scene) {
+        for (let i = 0; i < count; i++) {
+          const star = document.createElement("i");
+          const x = Math.floor(Math.random() * window.innerWidth);
+          const y = Math.floor(Math.random() * window.innerHeight);
+          const duration = Math.random() * 10;
+          const size = Math.random() * 2;
+
+          star.style.left = `${x}px`;
+          star.style.top = `${y}px`;
+          star.style.width = `${1 + size}px`;
+          star.style.height = `${1 + size}px`;
+
+          star.style.animationDuration = `${5 + duration}s`;
+          star.style.animationDelay = `${1 + duration}s`;
+
+          scene.appendChild(star);
+        }
+      }
+    };
+
+    createStars();
+  }, []);
+
+  return (
+    <div className="scene">
+      <div className="moon"></div>
+      <img src={forest} className="forest" alt="Forest" />
+      <img src={cloud1} className="cloud1" alt="Cloud 1" />
+      <img src={cloud2} className="cloud2" alt="Cloud 2" />
+      <img src={cloud3} className="cloud3" alt="Cloud 3" />
+      <img src={cloud1} className="cloud4" alt="Cloud 4" />
+      <img src={cloud2} className="cloud5" alt="Cloud 5" />
+      <img src={cloud3} className="cloud6" alt="Cloud 6" />
+      <div className="torch-container">
+        <img src={fireTorch} className="torch" alt="Torch" />
+        <p className="tooltip-text">Ça va faire tout noir !!</p>
+        <div className="bubbles">
+          <div className="bubble bubble1">TG!!</div>
+          <div className="bubble bubble2">TG!!</div>
+          <div className="bubble bubble3">Ta gueule!!</div>
+          <div className="bubble bubble4">TG!!</div>
+          <div className="bubble bubble5">Taaaa GUEULE!!</div>
+          <div className="bubble bubble6">TG!!</div>
+          <div className="bubble bubble7">Ta gueuleuuuuhhh!!</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Night;
